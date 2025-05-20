617ee81672bc70a173a99a01c692aaee05a1bc22
Leslie-Wong-H <79917148leslie@gmail.com>
2025-05-20 14:35:03 +0800

    Fix ids

--- anilist.js
+++ anilist.js
@@ -56,7 +56,10 @@ class ProxyFetcher {
 }
 
 export default async (req, res) => {
-  const { query = "", variables: ids = [] } = req.body;
+  const {
+    query = "",
+    variables: { ids = [] },
+  } = req.body;
 
   try {
     const fetcher = new ProxyFetcher();
@@ -74,7 +77,7 @@ export default async (req, res) => {
         //   query:
         //     "query ($ids: [Int]) { Page(page: 1, perPage: 50) { media(id_in: $ids, type: ANIME) { id title { native romaji english } type format status startDate { year month day } endDate { year month day } season episodes duration source coverImage { large medium } bannerImage genres synonyms studios { edges { isMain node { id name siteUrl } } } isAdult externalLinks { id url site } siteUrl } } }",
         query: query,
-        variables: { ids: ids },
+        variables: { ids: [...ids] },
       }),
       // agent: agent,
     }).catch((err) => console.error(err));
